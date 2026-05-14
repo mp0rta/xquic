327bac8ba26b2dbda3d01a473baf1878b2f81579
Noriyuki Kurosu <3p0rta26@gmail.com>
2026-05-14 09:40:13 +0900

    test(mp21): fold DoS continuous test into hard_cap stress (single test, 10 iterations)

--- tests/unittest/xqc_test_path_hard_cap.h
+++ tests/unittest/xqc_test_path_hard_cap.h
@@ -12,6 +12,5 @@ void test_path_create_no_heavy_state_on_validation_fail(void);
 void test_path_create_hard_cap_stress(void);
 void test_conn_stats_dynamic_paths_info(void);
 void test_dos_peer_init_max_path_id_max_valid(void);
-void test_dos_continuous_path_requests_capped(void);
 
 #endif /* XQC_TEST_PATH_HARD_CAP_H */
